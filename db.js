0da075cf12a710f8ea73b12d932dd2c13cb545ad
Ryandesigns09 <58941197+Ryandesigns09@users.noreply.github.com>
2022-07-11 12:57:59 -0600

    1

--- db.js
+++ db.js
@@ -8,13 +8,13 @@ var db = new sqlite3.Database('./var/db/todos.db');
 db.serialize(function() {
   db.run("CREATE TABLE IF NOT EXISTS users ( \
     id INTEGER PRIMARY KEY, \
-    user_id INTEGER NOT NULL, \
     username TEXT, \
     name TEXT, \
     battlebadge TEXT, \
     whitelist TEXT DEFAULT no, \
+     user_id text NOT NULL, \
     provider TEXT NOT NULL, \
-    subject TEXT NOT NULL, \
+    subject TEXT NOT NULL \
   )").run("ALTER TABLE users ADD COLUMN address TEXT", function(err){
     if(!err){
       console.log("column added");
